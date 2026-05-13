07a274b46e206aa642caa9e616f50c412175ab68
Keira <66428305+PuddlePumpkin@users.noreply.github.com>
2026-05-13 03:39:34 -0500

    make install.bat

---
+++ install.bat
@@ -0,0 +1,11 @@
+@echo off
+setlocal
+set "DEST_DIR=C:\Program Files (x86)\World of Warcraft\_classic_era_\Interface\AddOns\KwaKillCounter"
+if not exist "%DEST_DIR%" (
+    mkdir "%DEST_DIR%"
+)
+copy "%~dp0KwaKillCounter.lua" "%DEST_DIR%\"
+copy "%~dp0KwaKillCounter.toc" "%DEST_DIR%\"
+
+echo Files copied successfully to %DEST_DIR%
+pause
